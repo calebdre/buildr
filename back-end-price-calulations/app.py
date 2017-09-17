c3f039f5581f1ef944f1167f78cbd4370a36b435
Caleb Lewis <caleb_dre@yahoo.com>
2017-09-17 06:43:28 -0400

    handling not finding an item

--- back-end-price-calulations/app.py
+++ back-end-price-calulations/app.py
@@ -17,24 +17,32 @@ def findBestItem(items):
 	
 	infos = []
 	for item in items:
-		searchUrl = "http://www.homedepot.com/b/N-5yc1vZbwo5o/Ntk-semanticsearch/Ntt-{}}}?NCNI-5".format(item)
-		html = pq(url=searchUrl)
-		firstProduct = html("#products .pod-plp__container .js-pod-0")
+		try:
+			searchUrl = "http://www.homedepot.com/b/N-5yc1vZbwo5o/Ntk-semanticsearch/Ntt-{}}}?NCNI-5".format(item)
+			html = pq(url=searchUrl)
+			firstProduct = html("#products .pod-plp__container .js-pod-0")
 
-		info = {}
-		info["imageUrl"] = firstProduct("img").attr("src")
-		info["productName"] = firstProduct(".pod-plp__description").text()
-		info["url"] = "http://www.homedepot.com" + firstProduct(".pod-plp__description a").attr("href")
-		info["productId"] = info["url"].split("/")[-1]
-		info["addToCartLink"] = firstProduct(".pod-plp__atc-bttn a").attr("href")
-		info["productModel"] = firstProduct(".pod-plp__model").text()
-		info["numOfReviews"] = firstProduct(".pod-plp__ratings a").text()[1:][:-1]
-		priceSplitted = firstProduct(".price__wrapper .price").text().split(" ")
-		info["price"] = priceSplitted[0] + priceSplitted[1] + "." + priceSplitted[2]
-		info["priceFloat"] = float(priceSplitted[1] + "." + priceSplitted[2])
-		infos.append(info)
+			info = {}
+			info["imageUrl"] = firstProduct("img").attr("src")
+			info["productName"] = firstProduct(".pod-plp__description").text()
+			info["url"] = "http://www.homedepot.com" + firstProduct(".pod-plp__description a").attr("href")
+			info["productId"] = info["url"].split("/")[-1]
+			info["addToCartLink"] = firstProduct(".pod-plp__atc-bttn a").attr("href")
+			info["productModel"] = firstProduct(".pod-plp__model").text()
+			info["numOfReviews"] = firstProduct(".pod-plp__ratings a").text()[1:][:-1]
+			priceSplitted = firstProduct(".price__wrapper .price").text().split(" ")
+			info["price"] = priceSplitted[0] + priceSplitted[1] + "." + priceSplitted[2]
+			info["priceFloat"] = float(priceSplitted[1] + "." + priceSplitted[2])
+			infos.append(info)
+		except:
+			infos.append({"found":False})
 	
-	totalPrice = sum([i["priceFloat"] for i in infos])
+	totalPrice = 0
+	
+	for i in infos:
+		if "priceFloat" in i:
+			totalPrice = totalPrice + i["priceFloat"]
+	
 
 	response = {}
 	response["totalPrice"] = totalPrice
