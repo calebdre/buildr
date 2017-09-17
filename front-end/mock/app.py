ebb4ea622ae58b4a0836de002a35a15e5a8a761c
luis <ferrerluis97@gmail.com>
2017-09-17 03:25:34 -0400

    Do not provide instructions_url in mock API if there are children left

--- front-end/mock/app.py
+++ front-end/mock/app.py
@@ -42,7 +42,7 @@ def mock_data(n):
         project = {
             'name': fake.sentence(2),
             'picture_url': fake.image_url(),
-            'instructions_url': fake.url(),
+            'instructions_url': fake.url() if n - 1 <= 0 else None,
             'children': mock_data(n - 1),
             'materials': mock_materials() if n - 1 <= 0 else []
         }
