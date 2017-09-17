c8d4a6af39c8f045c11cc3d158ae24ff0b2e5949
Nishant Sinha <nishnha@gmail.com>
2017-09-17 01:43:38 -0400

    Added template rendering

---
+++ front-end/js/main.js
@@ -0,0 +1,24 @@
+let loadProjects = function(projects) {
+	console.log(projects);
+	if (projects) {
+		for (let project in projects) {
+			renderProject(project);
+		}
+	}
+}
+
+let renderProject = function(project) {
+	$.get('project.mustache', function (template) {
+		Mustache.parse(template);
+		let renderedProject = Mustache.render(template, project);
+		console.log(renderedProject);
+
+		$(".row").append(renderedProject);
+	});
+};
+
+$.get("http://127.0.0.1:5000/mock", function(projects) {
+	projects.forEach(function(project) {
+		renderProject(project)
+	});
+});
