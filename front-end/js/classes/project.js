d0e95c37d010c3925ea756ee42d29ee8ccd50521
luis <ferrerluis97@gmail.com>
2017-09-17 01:49:37 -0400

    Added top() static function for Project

--- front-end/js/classes/project.js
+++ front-end/js/classes/project.js
@@ -3,11 +3,21 @@ class Project {
     this.name = name;
     this.picture_url = picture_url;
     this.instructions_url = instructions_url;
-    this.children = children.map((child) => Project(child.name, child.children, child.materials, picture_url, instructions_url));
-    this.materials = materials.map((material) => Material(material.name, material.price, material.quantity, material.hd_id));
+    this.children = children.map((child) => new Project(child.name, child.picture_url, child.instructions_url, child.children, child.materials));
+    this.materials = materials.map((material) => new Material(material.name, material.price, material.quantity, material.hd_id));
   }
 
-  static all() {
-    // to do
+  static get constants() {
+    return {
+      API_URL: 'http://localhost:5000/mock'
+    }
+  }
+
+  static top() {
+    // Returns a Promise object. Use that promise as `promise.then((projects) => { ... })`
+    return Promise.resolve($.get(Project.constants.API_URL))
+      .map((project) => {
+        return new Project(project.name, project.picture_url, project.instructions_url, project.children, project.materials);
+      });
   }
 }
