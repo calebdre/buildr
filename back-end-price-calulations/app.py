ff9aba56e1a30e054cb9b59eaed61a77f2dc0604
Caleb Lewis <caleb_dre@yahoo.com>
2017-09-16 22:36:50 -0400

    added scraped top product info

--- back-end-price-calulations/app.py
+++ back-end-price-calulations/app.py
@@ -6,8 +6,16 @@ def findBestItem(item):
 	searchUrl = "http://www.homedepot.com/b/N-5yc1vZbwo5o/Ntk-semanticsearch/Ntt-{}}}?NCNI-5".format(item)
 	html = pq(url=searchUrl)
 	firstProduct = html("#products .pod-plp__container .js-pod-0")
-	image = firstProduct("img")
-	print(image.attr("src"))
+
+	info = {}
+	info["imageUrl"] = firstProduct("img").attr("src")
+	info["productName"] = firstProduct(".pod-plp__description").text()
+	info["url"] = "http://www.homedepot.com" + firstProduct(".pod-plp__description a").attr("href")
+	info["addToCartLink"] = firstProduct(".pod-plp__atc-bttn a").attr("href")
+	info["productModel"] = firstProduct(".pod-plp__model").text()
+	info["numOfReviews"] = firstProduct(".pod-plp__ratings a").text()[1:][:-1]
+
+	return json.dumps(info, ensure_ascii=False)
 
 
-findBestItem("hammer")
+print(findBestItem("hammer"))
