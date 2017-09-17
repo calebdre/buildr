46f70128e4664723c16014eebc3d6418376f9057
Caleb Lewis <caleb_dre@yahoo.com>
2017-09-17 01:00:07 -0400

    added pricefloat and totalprice

--- back-end-price-calulations/app.py
+++ back-end-price-calulations/app.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from flask import Flask
 from pyquery import PyQuery as pq
 
 def findBestItem(items):
@@ -16,14 +17,21 @@ def findBestItem(items):
 		info["imageUrl"] = firstProduct("img").attr("src")
 		info["productName"] = firstProduct(".pod-plp__description").text()
 		info["url"] = "http://www.homedepot.com" + firstProduct(".pod-plp__description a").attr("href")
+		info["productId"] = "http://www.homedepot.com" + info["url"].split("/")[0][-1:0]
 		info["addToCartLink"] = firstProduct(".pod-plp__atc-bttn a").attr("href")
 		info["productModel"] = firstProduct(".pod-plp__model").text()
 		info["numOfReviews"] = firstProduct(".pod-plp__ratings a").text()[1:][:-1]
 		priceSplitted = firstProduct(".price__wrapper .price").text().split(" ")
 		info["price"] = priceSplitted[0] + priceSplitted[1] + "." + priceSplitted[2]
+		info["priceFloat"] = float(priceSplitted[1] + "." + priceSplitted[2])
 		infos.append(info)
+	
+	totalPrice = sum([i["priceFloat"] for i in infos])
 
-	return json.dumps(infos, ensure_ascii=False)
+	response = {}
+	response["totalPrice"] = totalPrice
+	response["items"] = infos
+	return json.dumps(response, ensure_ascii=False)
 
 
 print(findBestItem(["hammer", "nail", "2x4 wood"]))
@@ -31,9 +39,10 @@ print(findBestItem(["hammer", "nail", "2x4 wood"]))
 
 def findStoreID(lat_lon): # format: ("Latitude,Longitude")
 	urlOpen = "http://www.homedepot.com/l/search/" + lat_lon +"/full/"
-		html = pq(url=urlOpen)
-			storeID = html(".sfstorename:eq(0)").text().split("#", 1)[1]
-    return(storeID)
+	html = pq(url=urlOpen)
+	storeID = html(".sfstorename:eq(0)").text().split("#", 1)[1]
+    
+	return(storeID)
 
 
 def returnAsileNum(location, productIDs):
@@ -45,3 +54,9 @@ def returnAsileNum(location, productIDs):
 	parsed_json = json.loads(asileNumJson)
 	return(parsed_json["storeSkus"][0]["aisleBayInfo"]["aisle"])
 
+
+
+# app = Flask(__name__)
+
+# app.route("/check_prices")
+# def 
